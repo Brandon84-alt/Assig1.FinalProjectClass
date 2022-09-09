3d8e36a0729323ec2cfb14613f2498c8e93a4dec
Brandon84-alt <brandonlevis84@gmail.com>
2022-09-09 18:01:33 -0400

    Section Testing --Brandon

---
+++ section-service/src/test/java/com/labrielevis/sectionservice/presentationlayer/SectionControllerTest.java
@@ -0,0 +1,10 @@
+package com.labrielevis.sectionservice.presentationlayer;
+
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+@DataMongoTest
+
+class SectionControllerTest {
+
+
+}
